88fb4a5567254151f7fe144130d41293185b5ffa
UnitySam Rung <unitysamrung@gmail.com>
2016-07-12 02:17:12 +0700

    add

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require ('express');
+var app = express ();
+var server = require ('http').createServer (app);
+var io = require ('socket.io').listen (server);
+
+app.set ('port', process.env.PORT || 3000);
+
+app.get ('/', getRoot);
+
+function getRoot (req, res) {
+  res.send ('Hello Express');
+}
+
+server.listen (app.get ('port'), function () {
+  console.log ("Server is running")
+});
